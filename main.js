d6889430fa5341aa5a5a6c25acd70820a08d16fc
martypnz <141710574+martypnz@users.noreply.github.com>
2023-09-19 18:02:55 +1200

    java

---
+++ main.js
@@ -0,0 +1,48 @@
+console.log("hello world");
+console.log(1000);
+console.log(3000 + 2000);
+console.log(true);
+
+//variables
+var num;
+num = 5000;
+console.log(num + num);
+
+//var name="hello"
+//var name2="world"
+
+//console.log(name+name2)
+
+//console.log(1000+1000);
+
+//var myname='martin'
+var myName = "some name";
+var myAwesomeCode = 1000;
+
+//array or list
+var fruits = ["apple", "orange", "kiwi"];
+var random = [1, "hello world", 1000, true, 0];
+
+//instead of var we now use const and let
+
+//var x;
+//x = 100;
+//console.log(x);
+//x = hello;
+//console.log(x);
+//var x = true;
+//const and let
+const x = "hello";
+
+//let y
+//y=20
+//y='hello world'
+
+//console.log(document.head)
+//document.body.style.backgroundColor="#f11"
+//document.getElementById('heading').style.color="#f11"
+//document.getElementById('heading').style.FontSize='5em'
+
+const h2= document.getElementById("heading");
+h1.style.color="red"
+h1.style.FontSize="5em"
